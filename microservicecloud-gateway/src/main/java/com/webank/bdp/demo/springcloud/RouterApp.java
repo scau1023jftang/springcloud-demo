6c4011c4ad906ceb922a2dc0bb6dd25bd00f0f5a
scau1023jftang <40909410+scau1023jftang@users.noreply.github.com>
2018-12-15 10:23:16 +0800

    add filter

--- microservicecloud-gateway/src/main/java/com/webank/bdp/demo/springcloud/RouterApp.java
+++ microservicecloud-gateway/src/main/java/com/webank/bdp/demo/springcloud/RouterApp.java
@@ -19,13 +19,17 @@ public class RouterApp {
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        Function<PredicateSpec, Route.Builder> fn = new Function<PredicateSpec, Route.Builder>() {
+/*        Function<PredicateSpec, Route.Builder> fn = new Function<PredicateSpec, Route.Builder>() {
 
             public Route.Builder apply(PredicateSpec t) {
                 t.path("/api/upload");
                 return t.uri("http://localhost:8001");
             }
         };
-        return builder.routes().route(fn).build();
+        return builder.routes().route(fn).build();*/
+        return builder.routes().route(p -> p
+                .path("/api/upload")
+                .filters(f -> f.addRequestHeader("bdp-tickid-id","123456"))
+                .uri("http://localhost:8001")).build();
     }
 }
